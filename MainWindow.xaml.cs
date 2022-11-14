77dcc24e2585e2a9c8ed287a6c62c43c6a890bea
TehTemM@gmail.com <TehTemM@gmail.com>
2022-11-14 13:09:45 +1000

    3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,7 +22,32 @@ namespace Wpf_Chemeris_PR4._1
     {
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+        private void Sgen_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(101);
+            lab.Text = i.ToString();
+        }
+        private void Ugaday_Click(object sender, RoutedEventArgs e)
+        {
+            int i = int.Parse(lab.Text);
+            int test = Convert.ToInt32(rofl.Text);
+            if (i == test)
+            {
+                MessageBox.Show("Угадал, возьми с полки пиражок");
+            }
+            else if (i > test)
+            {
+                MessageBox.Show("Маленькое число, попробуй ещё раз");
+            }
+            else
+            {
+                MessageBox.Show("Много хочешь, пробуй ещё");
+            }
         }
     }
 }
+
